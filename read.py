b9d644c8038c14a20d9c5c3d79e0644e3d841ae2
Markus Brown <markus04b@gmail.com>
2026-03-03 00:28:28 +0000

    other

--- read.py
+++ read.py
@@ -3,9 +3,21 @@ from models import engine, Bio
 import pandas as pd
 
 with Session(engine) as session:
-    # `Bio` is an alias for `Roster`; select fields that exist in the model
     statement = select(Bio.player, Bio.pos)
     records = session.exec(statement).all()
 
 records_df = pd.DataFrame(records, columns=["player", "pos"])
 print(records_df)
+
+
+# another example:
+
+# with Session(engine) as session:
+#     statement = (
+#         select(Bio.first_name, Bio.last_name, Bio.position)
+#         .order_by(Bio.position, Bio.last_name)
+#     )
+#     records = session.exec(statement).all()
+    
+# records_df = pd.DataFrame(records, columns=["first_name", "last_name", "position"])
+# print(records_df)
